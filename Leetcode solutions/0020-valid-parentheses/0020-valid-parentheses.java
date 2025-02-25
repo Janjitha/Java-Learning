7d8bd46076dc23fa72ff971a9c53c10ece50acb2
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-02-25 23:03:16 +0530

    Update 0020-valid-parentheses.java

--- Leetcode solutions/0020-valid-parentheses/0020-valid-parentheses.java
+++ Leetcode solutions/0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,33 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
-        int n = s.length();
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            if ((s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') && st.isEmpty()) 
-            {
-                return false;
-            }
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-                st.push(s.charAt(i));
-            } else {
-                if (s.charAt(i) == ')' && st.peek() == '(') {
-                    st.pop();
-                } else if (s.charAt(i) == ']' && st.peek() == '[') {
-                    st.pop();
-                } else if (s.charAt(i) == '}' && st.peek() == '{') {
-                    st.pop();
-                }
-
-                else {
-                    return false;
-                }
-
-            }
+         while(s.contains("()") || s.contains("[]") || s.contains("{}")){
+            s=s.replace("()","");
+            s=s.replace("[]","");
+            s=s.replace("{}","");
         }
-        if (st.size() == 0) {
-            return true;
-        }
-        return false;
-
+        return s.isEmpty();
     }
 }
