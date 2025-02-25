7a9d91c7820ca5543b97bdd8173059f325193c4a
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-02-25 23:05:30 +0530

    Update 0029-divide-two-integers.java

--- Leetcode solutions/0029-divide-two-integers/0029-divide-two-integers.java
+++ Leetcode solutions/0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,17 +1,9 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        int ans=dividend/divisor;
-        
-        if(dividend==-2147483648 && divisor==1)
-        {
-            return -2147483648;
+        if (dividend == -2147483648 && divisor == -1) {
+            return (2147483647);
         }
-        if(dividend==-2147483648 && divisor==-1)
-        {
-            return 2147483647;
-        }
-         
-         return ans;
-        
+        int res = dividend / divisor;
+        return res;
     }
 }
