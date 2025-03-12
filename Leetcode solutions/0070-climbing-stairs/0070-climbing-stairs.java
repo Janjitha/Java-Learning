25e427c0e304a1229cb8ee930cc1284aa04b6c20
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-12 22:07:10 +0530

    Update 0070-climbing-stairs.java

--- Leetcode solutions/0070-climbing-stairs/0070-climbing-stairs.java
+++ Leetcode solutions/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,27 +1,16 @@
 class Solution {
-    public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        int ans=fibo(n,dp);
-        return ans;
-        
-    }
-    int fibo(int n,int dp[])
-    {
-        if(n==1)
-        return 1;
-        if(n==2)
-        {
-            return 2;
+    public int climbStairs(int n) {/*
+                                    * if (n == 1) {
+                                    * return 1;
+                                    * }
+                                    */
+        int a = 1, b = 1;
+        for (int i = 2; i <= n; ++i) {
+            int current = a + b;
+            b = a;
+            a = current;
         }
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        else
-        {
-            dp[n]= fibo(n-1,dp)+fibo(n-2,dp);
-            return dp[n];
-        }
+        return a;
     }
 }
+
