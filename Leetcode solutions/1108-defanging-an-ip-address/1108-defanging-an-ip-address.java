694b34b19008ac8208fa3beb68e3e55ebd9e1989
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 13:15:45 +0530

    Update 1108-defanging-an-ip-address.java

--- Leetcode solutions/1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ Leetcode solutions/1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,16 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder ans=new StringBuilder();
-        for(char character : address.toCharArray()){
-             if (character == '.') {
-                ans.append("[.]");
-            }
-            else{
-                ans.append(character);
-            }
-            
-        }
-        return ans.toString();
+        return address.replace(".","[.]");
     }
 }
-
