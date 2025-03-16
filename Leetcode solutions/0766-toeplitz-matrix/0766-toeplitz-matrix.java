202aa326d5f1a37316f86c554d29c7dcdce9a047
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 14:33:28 +0530

    Update 0766-toeplitz-matrix.java

--- Leetcode solutions/0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ Leetcode solutions/0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        if(!toeplitz(0, 0, matrix)) return false;
-        for(int i = 0; i < matrix.length; i++){
-            if(!toeplitz(i, 0, matrix)) return false;
-        }
-        for(int j = 0; j < matrix[0].length; j++){
-            if(!toeplitz(0, j, matrix)) return false;
-        }
-        return true;
-    }
-    
-    private boolean toeplitz(int i, int j, int[][] matrix){
-        while(i+1 < matrix.length && j+1 < matrix[0].length){
-            if(matrix[i+1][j+1] != matrix[i++][j++]) return false;
-        }
-        return true;
+        int r=matrix.length;
+        int c=matrix[0].length;
+	    for(int i=1;i<r;i++) {
+	    	for(int j=1;j<c;j++) {
+	    			if(matrix[i][j]!=matrix[i-1][j-1]) {
+	    				return false;
+	    			}
+	    	} 
+	    }
+	    return true;
     }
 }
