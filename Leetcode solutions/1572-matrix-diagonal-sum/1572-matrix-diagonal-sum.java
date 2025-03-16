6f8a3f7e0316b32d792e22cd70623a45aa1e7eaf
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 12:39:27 +0530

    Update 1572-matrix-diagonal-sum.java

--- Leetcode solutions/1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ Leetcode solutions/1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,5 +1,28 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int sum = 0;
+        int sum1 = 0;
+        int total;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j && i + j != n - 1) {
+                    sum = sum + mat[i][j];
+                }
+                if (i + j == n - 1) {
+                    sum1 = sum1 + mat[i][j];
+                }
+            }
+        }
+        total = sum + sum1;
+        return total;
+    }
+}
+
+/*
+class Solution {
+    public int diagonalSum(int[][] mat) {
         int matLen = mat.length;
         int ans = 0;
         for(int i=0; i<matLen/2; i++){
@@ -10,3 +33,4 @@ class Solution {
         return ans;
     }
 }
+*/
