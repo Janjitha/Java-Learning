f923824d0d79b84cd92d4cfcbd660550ca1c4b17
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 11:21:03 +0530

    Update 0283-move-zeroes.java

--- Leetcode solutions/0283-move-zeroes/0283-move-zeroes.java
+++ Leetcode solutions/0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,13 @@
 class Solution {
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
     public void moveZeroes(int[] nums) {
-        if(nums.length == 1) return;
-        int count = 0;
-        int indexToSwap = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] == 0) count++;
-            else{
-                swap(nums,i,indexToSwap);
-                indexToSwap++;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int t = nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+                j++;
             }
         }
-        return;
-        
-    }
+    }//System.out.print(nums+"");
 }
