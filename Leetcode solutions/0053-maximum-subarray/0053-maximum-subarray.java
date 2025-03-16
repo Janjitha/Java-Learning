550be7bd837781016e6c50914fc863740b94e0ba
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:44:03 +0530

    Update 0053-maximum-subarray.java

--- Leetcode solutions/0053-maximum-subarray/0053-maximum-subarray.java
+++ Leetcode solutions/0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // code here
-         // dp[i] := the maximum sum subarray ending in i
-    int[] dp = new int[nums.length];
-
-    dp[0] = nums[0];
-    for (int i = 1; i < nums.length; ++i)
-      dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
-
-    return Arrays.stream(dp).max().getAsInt();
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>max){
+                max=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }return max;
     }
 }
