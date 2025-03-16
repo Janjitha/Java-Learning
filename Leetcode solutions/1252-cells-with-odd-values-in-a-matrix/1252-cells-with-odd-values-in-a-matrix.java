3340e66b88b956043a537d6131e3b851c7a625ed
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:51:30 +0530

    Update 1252-cells-with-odd-values-in-a-matrix.java

--- Leetcode solutions/1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ Leetcode solutions/1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -1,5 +1,31 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
+        int a[][] = new int[m][n];
+        for (int i = 0; i < indices.length; i++) {
+            int row = indices[i][0];
+            int col = indices[i][1];
+            for (int j = 0; j < n; j++) {
+                a[row][j]++;
+            }
+            for (int k = 0; k < m; k++) {
+                a[k][col]++;
+            }
+        }
+        int count = 0;
+        for (int k = 0; k < m; k++) {
+            for (int z = 0; z < n; z++) {
+                if (a[k][z] % 2 != 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+/*
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
         int ans = 0;
         // rows[i] and cols[i] :=
         // true (flipped even times) / false (flipped odd times)
@@ -19,3 +45,4 @@ class Solution {
         return ans;
     }
 }
+*/
