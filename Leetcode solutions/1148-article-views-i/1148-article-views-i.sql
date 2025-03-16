963800f538d9bde810975f36d2131d7297ddb4b3
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 12:38:47 +0530

    Update 1148-article-views-i.sql

--- Leetcode solutions/1148-article-views-i/1148-article-views-i.sql
+++ Leetcode solutions/1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,12 @@
 # Write your MySQL query statement below
+select distinct author_id as id
+from views
+where author_id = viewer_id
+order by author_id;
+
+/*
+# Write your MySQL query statement below
 SELECT DISTINCT author_id as id FROM Views 
 WHERE author_id = viewer_id 
 ORDER BY id ASC
+*/
