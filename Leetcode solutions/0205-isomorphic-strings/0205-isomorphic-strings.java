edf18a6d879747165936ed18ea087bca402d0cd1
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 11:20:17 +0530

    Update 0205-isomorphic-strings.java

--- Leetcode solutions/0205-isomorphic-strings/0205-isomorphic-strings.java
+++ Leetcode solutions/0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,21 +1,15 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> map = new HashMap<>();
-        for(int i = 0;i<s.length();i++){
-            if(!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),t.charAt(i));
-            }else if(map.get(s.charAt(i))!=t.charAt(i)){
+        int a_s[]=new int[256];
+        int a_t[]=new int[256];
+        for(int i=0;i<s.length();i++){
+            char s1=s.charAt(i);
+            char t1=t.charAt(i);
+            if(a_s[s1]==0 && a_t[t1]==0)
+                a_s[s1]=t1;
+                a_t[t1]=s1;
+            if(a_s[s1]!=t1 || a_t[t1]!=s1)
                 return false;
-            }
-        }
-        map.clear();
-        for(int i = 0;i<s.length();i++){
-            if(!map.containsKey(t.charAt(i))){
-                map.put(t.charAt(i),s.charAt(i));
-            }else if(map.get(t.charAt(i))!=s.charAt(i)){
-                return false;
-            }
-        }
-        return true;
+        }return true;
     }
 }
