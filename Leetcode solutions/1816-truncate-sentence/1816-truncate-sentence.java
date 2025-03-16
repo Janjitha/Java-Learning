59f149dc62b8ffab4a25ebcf0ddc3279bbd35108
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 14:36:02 +0530

    Update 1816-truncate-sentence.java

--- Leetcode solutions/1816-truncate-sentence/1816-truncate-sentence.java
+++ Leetcode solutions/1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,17 +1,39 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        int c=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)==' ')
-            {
-               c++; 
-                if(c==k)
-                {
-                    return(s.substring(0,i));
+        int count = 0, i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == ' ') {
+                count++;
+                if (count == k){
+                    break;
                 }
             }
-        }
-        return s;
+            i++;
+        } 
+        return s.substring(0, i);
+        //return String.join(" ", Arrays.copyOfRange(a, 0, k));
     }
 }
+
+/*
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String a[]=s.split(" ");
+        String result = a[0];  
+        for (int i = 1; i < k; i++) {
+            result += " " + a[i];  
+        }
+        return result;
+        //return String.join(" ", Arrays.copyOfRange(a, 0, k));
+    }
+}
+*/
+
+/*
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String a[]=s.split(" ");
+        return String.join(" ", Arrays.copyOfRange(a, 0, k));
+    }
+}
+*/
