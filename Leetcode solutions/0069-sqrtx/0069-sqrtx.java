d1bbc56af93ab87f8c8ffe4ac2678c29f900b4c4
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:44:54 +0530

    Update 0069-sqrtx.java

--- Leetcode solutions/0069-sqrtx/0069-sqrtx.java
+++ Leetcode solutions/0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-    if(x <= 1) {
-            return x;
-        }
-        
-        int start = 1, end = (x/2)+1, sqrt = 0; 
-        long mid = 0;
-
-        while(start <= end) {
-            mid = (start+end)/2;
-            if(mid*mid == x) {
-                return (int) mid;
-            } else if(mid*mid < x) {
-                start = (int) mid + 1;
-                sqrt = (int) mid;
-            } else {
-                end = (int) mid - 1;
-            }
-        }
-
-        return sqrt;
+        int res = (int)Math.pow(x,0.5);
+        return res;
+        //int res = (int)Math.sqrt(x);
+        //return res;
     }
 }
+
+/*
+class Solution {
+    public int mySqrt(int x) {
+        while(x>0){
+            return (int)Math.pow(x,0.5);
+        }return 0;
+    }
+}
+*/
