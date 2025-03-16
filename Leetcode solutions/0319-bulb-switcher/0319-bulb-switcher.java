81076a1f1f0f60a7d77b1b39b5e879012a562d47
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 12:04:49 +0530

    Update 0319-bulb-switcher.java

--- Leetcode solutions/0319-bulb-switcher/0319-bulb-switcher.java
+++ Leetcode solutions/0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,5 +1,12 @@
 class Solution {
     public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
+
+/*
+class Solution {
+    public int bulbSwitch(int n) {
         int count = 0;
         for(int i = 1; i*i <= n; i++)
         {
@@ -8,3 +15,4 @@ class Solution {
         return count;
     }
 }
+*/
