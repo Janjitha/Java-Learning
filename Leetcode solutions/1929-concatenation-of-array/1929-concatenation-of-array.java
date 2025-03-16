9f60afccbfffff917126088d63efddcc5cc65f38
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 13:19:36 +0530

    Update 1929-concatenation-of-array.java

--- Leetcode solutions/1929-concatenation-of-array/1929-concatenation-of-array.java
+++ Leetcode solutions/1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int n=nums.length;
-        int ans[]=new int [n+n];
+        final int n = nums.length;
+        int k[] = new int[n*2];
         for(int i=0;i<n;i++){
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
+            k[i]=k[i+n];
+            k[i]=nums[i];
         }
-        return ans;
-        
+        return k;
     }
 }
