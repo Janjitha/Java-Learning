8bf1f633658b5dffe9ed329e79acc08513d0ce2f
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 12:05:38 +0530

    Update 0566-reshape-the-matrix.java

--- Leetcode solutions/0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ Leetcode solutions/0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,5 +1,29 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int x=mat.length;
+        int y=mat[0].length;
+        if(x*y!=r*c) return mat;
+        int a[][] = new int[r][c];
+        int row=0,col=0,index=0;
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                // if(col==c){
+                //     col=0;
+                //     row++;
+                // }
+                // a[row][col]= mat[i][j];
+                // col++;
+                a[index/c][index%c]=mat[i][j];
+                index++;
+            }
+        }
+        return a;
+    }
+}
+
+/*
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
         int n=mat.length;
         int m=mat[0].length;
         if(m*n!=r*c)
@@ -11,8 +35,7 @@ class Solution {
         {
             ans[i/c][i%c]=mat[i/m][i%m];
         }
-        return ans;
-        
-        
+        return ans;        
     }
 }
+*/
