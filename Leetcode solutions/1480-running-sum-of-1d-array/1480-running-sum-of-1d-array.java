cdc4d0aaabf28050b10397478cf0b2b90d07b115
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 13:17:40 +0530

    Update 1480-running-sum-of-1d-array.java

--- Leetcode solutions/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ Leetcode solutions/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -8,3 +8,21 @@ class Solution {
         return k;
     }
 }
+
+/*
+class Solution {
+     public int numJewelsInStones(String jewels, String stones) {
+         List<Character>al =new ArrayList<>();
+         int count=0;
+     for(char jewel :jewels.toCharArray()){
+         al.add(jewel);      
+     }
+         for(char stone:stones.toCharArray()){
+             if(al.contains(stone)){
+                 count++;
+             }
+         }
+         return count;
+     }
+ }
+*/
