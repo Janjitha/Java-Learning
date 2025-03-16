ccc9ff9a386bc4e27fba3b150bb11938617ecc82
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 11:24:31 +0530

    Update 595-big-countries.sql

--- Leetcode solutions/0595-big-countries/595-big-countries.sql
+++ Leetcode solutions/0595-big-countries/595-big-countries.sql
@@ -1,2 +1,8 @@
+select name, population, area 
+from world
+where area>=3000000 or population >= 25000000;
+
+/*
 # Write your MySQL query statement below
 SELECT name,population,area from World WHERE area >= 3000000 OR population >= 25000000;
+*/
