e2618c8b6c1cd7209530622dd63cde6e1a11116c
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 14:38:21 +0530

    Update 2206-divide-array-into-equal-pairs.java

--- Leetcode solutions/2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ Leetcode solutions/2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-        int numOfPairs = nums.length/2;
-        int pairs = 0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++) {
-                if(nums[i] == nums[j] && nums[i] != 0 && nums[j] != 0){
-                    pairs++;
-                    nums[i] = 0;
-                    nums[j] = 0;
+        Arrays.sort(nums);
+        int count=0;
+        if(nums.length%2!=0){
+            return false;
+        }
+        else{
+            for(int i=0;i<nums.length;i=i+2){
+                if(nums[i]==nums[i+1]){
+                    count++;
                 }
             }
+            if(count==nums.length/2){
+                return true;
+            }
         }
-        return pairs == numOfPairs;
+        return false;
     }
 }
