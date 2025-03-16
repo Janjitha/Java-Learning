f9de9897bbf844c9693f07a161d7184a6e8471c0
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 11:25:34 +0530

    Update 709-to-lower-case.java

--- Leetcode solutions/0709-to-lower-case/709-to-lower-case.java
+++ Leetcode solutions/0709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,14 @@
 class Solution {
     public String toLowerCase(String s) {
+        s=s.toLowerCase();
+        return s;
+    }
+}
+
+/*
+class Solution {
+    public String toLowerCase(String s) {
         return s.toLowerCase();
     }
 }
+*/
