e62d6ed01bace108633edafab7fcc6b4014ac02e
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 14:35:01 +0530

    Update 1790-check-if-one-string-swap-can-make-strings-equal.java

--- Leetcode solutions/1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ Leetcode solutions/1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        int i=-1, j=-1;
-        int cnt=0;
-
-        for(int k=0; k<s1.length(); k++){
-            if(s1.charAt(k)!=s2.charAt(k)){
-                cnt++;
-                if(i==-1) i=k;
-                else if(j==-1) j=k;
+        int first = -1, second = -1;
+        int diffCount = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+            diffCount++;
+            if (first == -1) first = i;
+            else second = i;
             }
         }
-
-        if(cnt==0) return true;
-        else if(cnt==2 && s1.charAt(i)==s2.charAt(j) && s1.charAt(j)==s2.charAt(i)){
-            return true;
-        }
-
-
-        return false;
+        if (diffCount == 0) return true; 
+        if (diffCount == 2 && s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first))
+        return true;
+    return false;
     }
 }
+
+    
