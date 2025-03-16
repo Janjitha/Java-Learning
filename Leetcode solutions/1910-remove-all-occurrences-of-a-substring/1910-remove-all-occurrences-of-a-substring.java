36f53c4e76bf3090095690b3942ac275bd91867b
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 14:36:50 +0530

    Update 1910-remove-all-occurrences-of-a-substring.java

--- Leetcode solutions/1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ Leetcode solutions/1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,6 +1,17 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
+        while(s.contains(part)){
+            s=s.replaceFirst(part,"");
+        }
+        return s;
+    }
+}
+
+/*
+class Solution {
+    public String removeOccurrences(String s, String part) {
         while (s.contains(part)) s = s.replaceFirst(part, "");
         return s;
     }
 }
+*/
