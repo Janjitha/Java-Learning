3a9c57692e386157c2666c7798afae07273881a0
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 14:40:55 +0530

    Update 2544-alternating-digit-sum.java

--- Leetcode solutions/2544-alternating-digit-sum/2544-alternating-digit-sum.java
+++ Leetcode solutions/2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -1,9 +1,33 @@
 class Solution {
     public int alternateDigitSum(int n) {
+        int len=String.valueOf(n).length();
+        int arr[] = new int[len];
+        int j = len-1;
+		while(n>0){
+			int rem = n%10;
+			arr[j--] = rem;
+			n = n/10;
+		}
+        int even = 0;
+        int odd = 0;
+        int c = 0;
+		for (int i = 0; i < len; i++) {
+			if(c%2!=0){
+				odd += arr[i];
+			}else{
+				even+=arr[i];
+			}
+            c++;
+		}
+        return even - odd;
+        }
+}
+
+/*
+class Solution {
+    public int alternateDigitSum(int n) {
         int sum=0;
-
         String s = Integer.toString(n);
-
         char a[]=s.toCharArray();
         for(int i =0;i<a.length;i++)
         {
@@ -12,7 +36,7 @@ class Solution {
             else
             sum-=Character.getNumericValue(a[i]);
         }
-
         return sum;
     }
 }
+*/
