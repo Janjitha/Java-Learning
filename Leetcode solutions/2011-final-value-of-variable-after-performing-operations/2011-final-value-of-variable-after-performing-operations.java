1542fdf9308b201e53ff38d6427f7185fc61746d
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 13:20:26 +0530

    Update 2011-final-value-of-variable-after-performing-operations.java

--- Leetcode solutions/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ Leetcode solutions/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -1,5 +1,20 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
+        int res =0;
+        for(String i:operations){
+            if(i.charAt(1)=='+'){
+                res++;
+            }
+            else{
+                res--;
+            }
+        }return res;
+    }
+}
+
+/*
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
         int sum=0;
         for(int i=0;i<operations.length;i++){
              if(operations[i].equals("--X") || operations[i].equals("X--")) {
@@ -7,10 +22,9 @@ class Solution {
             }
             else{
                 sum+=1;
-            }
-            
+            } 
         }
-        return sum;
-        
+        return sum;    
     }
 }
+*/
