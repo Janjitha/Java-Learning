2e8bd95109a5aafca4203e3107bc3b9c077d8924
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 12:09:28 +0530

    Update 2652-sum-multiples.java

--- Leetcode solutions/2652-sum-multiples/2652-sum-multiples.java
+++ Leetcode solutions/2652-sum-multiples/2652-sum-multiples.java
@@ -1,17 +1,10 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        int sum = 0;
-        if((n == 1) || (n == 2))
-        {
-            return 0;
-        }
-        for(int i = 1; i <= n; i++)
-        {
-            if((i%3 == 0)||(i%5== 0)||(i%7== 0))
-            {
-                sum += i;
-            }
-        }
-        return sum;
+        int i=3,sum=0;
+        while(i<=n){
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum+=i;
+            }i++;
+        }return sum;
     }
 }
