dc1182ec3023d53e51bda1b3dbf41c66164e6d98
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 15:14:46 +0530

    Update 3274-check-if-two-chessboard-squares-have-the-same-color.java

--- Leetcode solutions/3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
+++ Leetcode solutions/3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        boolean c1 = (coordinate1.charAt(0) + coordinate1.charAt(1)) % 2 != 0;
-        boolean c2 = (coordinate2.charAt(0) + coordinate2.charAt(1)) % 2 != 0;
-        return c1 == c2;
+        int a = coordinate1.charAt(0)+coordinate1.charAt(1);
+        int b = coordinate2.charAt(0)+coordinate2.charAt(1);
+        return (a+b)%2==0;
     }
 }
