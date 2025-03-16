6308aae06c32e0c4a3d992cb1dcf2ad68412a571
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:06:06 +0530

    Update 0263-ugly-number.java

--- Leetcode solutions/0263-ugly-number/0263-ugly-number.java
+++ Leetcode solutions/0263-ugly-number/0263-ugly-number.java
@@ -1,37 +1,11 @@
 class Solution {
-    int check(int n)
-    {
-        if(n==1)
-        {
-            return 1;
-        }
-        if(n<=0)
-        {
-            return 0;
-        }
-        if(n%2==0)
-        {   
-            int a=n/2;
-            return check(a);
-        }
-        if(n%3==0)
-        {   
-            int b=n/3;
-            return check(b);
-        }
-        if(n%5==0)
-        {   
-            int c=n/5;
-            return check(c);
-        }
-        return 0;
-    }
     public boolean isUgly(int n) {
-        int ans=check(n);
-        if(ans!=1)
-        {
-            return false;
+       for(int a=2;a<6 && n>0;a++)
+       {
+        while(n%a==0){
+            n/=a;
         }
-        return true; 
+       } 
+       return n==1;
     }
 }
