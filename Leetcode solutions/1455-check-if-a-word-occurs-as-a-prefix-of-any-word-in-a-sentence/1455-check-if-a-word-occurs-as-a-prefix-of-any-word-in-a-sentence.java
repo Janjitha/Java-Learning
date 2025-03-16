9b71dcdf158327907baccd8f6391cab761ab1e33
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 13:16:21 +0530

    Update 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java

--- Leetcode solutions/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ Leetcode solutions/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,11 +1,11 @@
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
-        String words[] = sentence.split(" ");
-        for(int i=0;i<words.length;i++){
-            if(words[i].startsWith(searchWord)){
-                return i+1;
+        String[] r = sentence.split(" ");
+        for (int i = 0; i < r.length; i++) {
+            if (r[i].startsWith(searchWord)) {
+                return i + 1; 
             }
         }
-        return -1;
+        return -1; 
     }
 }
