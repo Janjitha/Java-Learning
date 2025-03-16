2bbf42680e00eca451d79d3d46ca69398000710d
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:46:37 +0530

    Update 0258-add-digits.java

--- Leetcode solutions/0258-add-digits/0258-add-digits.java
+++ Leetcode solutions/0258-add-digits/0258-add-digits.java
@@ -1,5 +1,18 @@
 class Solution {
     public int addDigits(int num) {
+        if(num>9){
+            num=num%9;
+            if(num==0){
+                return 9;
+            }
+        }return num;
+    }
+}
+
+/*
+class Solution {
+    public int addDigits(int num) {
         return 1 + (num - 1) % 9;  
     }
 }
+*/
