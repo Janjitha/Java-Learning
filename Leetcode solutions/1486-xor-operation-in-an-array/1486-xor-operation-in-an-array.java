63085a2c2e7d7e58114a7b5ba419f22f185df333
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:51:56 +0530

    Update 1486-xor-operation-in-an-array.java

--- Leetcode solutions/1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ Leetcode solutions/1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,8 +1,10 @@
 class Solution {
     public int xorOperation(int n, int start) {
-        int ans = 0;
-        for (int i = 0; i < n; ++i)
-            ans ^= start + 2 * i;
-        return ans;
+        int result = 0;
+        for (int i = 0; i < n; i++) {  
+            int num = start + 2 * i;  
+            result ^= num;  
+        }
+        return result;
     }
 }
