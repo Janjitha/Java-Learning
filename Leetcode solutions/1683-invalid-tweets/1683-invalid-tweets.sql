722f34c0997f8ae576a953cea8c87ad6ec568e3a
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 12:07:02 +0530

    Update 1683-invalid-tweets.sql

--- Leetcode solutions/1683-invalid-tweets/1683-invalid-tweets.sql
+++ Leetcode solutions/1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
+select tweet_id
+from tweets
+where length(content) > 15 
+
+/*
+# Write your MySQL query statement below
 SELECT tweet_id 
 FROM Tweets 
 where CHAR_LENGTH(content) > 15;
+*/
