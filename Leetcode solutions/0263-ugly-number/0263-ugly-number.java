1830b5293a2785f438715d4894bf2ac25de7d011
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:06:47 +0530

    Update 0263-ugly-number.java

--- Leetcode solutions/0263-ugly-number/0263-ugly-number.java
+++ Leetcode solutions/0263-ugly-number/0263-ugly-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isUgly(int n) {
-       for(int a=2;a<6 && n>0;a++)
-       {
-        while(n%a==0){
-            n/=a;
-        }
+       for(int a=2;a<6 && n>0;a++){
+           while(n%a==0){
+               n/=a;
+           }
        } 
        return n==1;
     }
