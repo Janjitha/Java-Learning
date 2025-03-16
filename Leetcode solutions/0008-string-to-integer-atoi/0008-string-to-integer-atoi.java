f19cd8fb5dc9017199f214fc389bcd75ae9f22b1
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:43:13 +0530

    Update 0008-string-to-integer-atoi.java

--- Leetcode solutions/0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ Leetcode solutions/0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,26 +1,27 @@
 class Solution {
     public int myAtoi(String s) {
-        // code here
-        s = s.strip();
-        if (s.isEmpty())
-            return 0;
-
-        final int sign = s.charAt(0) == '-' ? -1 : 1;
-        if (s.charAt(0) == '+' || s.charAt(0) == '-')
-            s = s.substring(1);
-
-        long num = 0;
-
-        for (final char c : s.toCharArray()) {
-            if (!Character.isDigit(c))
-                break;
-            num = num * 10 + (c - '0');
-            if (sign * num <= Integer.MIN_VALUE)
-                return Integer.MIN_VALUE;
-            if (sign * num >= Integer.MAX_VALUE)
-                return Integer.MAX_VALUE;
+        int sign = 1;
+       long ans = 0;
+       int ind = 0;
+       //Move when there is spaces
+       while(ind<s.length() && s.charAt(ind)==' '){
+        ind++;
+       }
+       //For finding sign of the integer value
+       if(ind<s.length() && (s.charAt(ind)=='-' || s.charAt(ind)=='+')){
+        sign=(s.charAt(ind)=='+')?1:-1;
+        ind++;
+       }
+       while(ind<s.length() && Character.isDigit(s.charAt(ind))){
+        ans = ans*10 + (int)(s.charAt(ind)-'0');
+        //For long values that it should be in the limit
+        if(sign==1 && ans>Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }else if(sign==-1 && ((-1*ans)<Integer.MIN_VALUE)){
+            return Integer.MIN_VALUE;
         }
-
-        return sign * (int) num;
+        ind++;
+       }
+       return (int)ans*sign;
     }
 }
