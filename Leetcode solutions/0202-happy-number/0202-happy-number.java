e80c56ee770cd1101222fed3493aa20645745fa3
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-16 08:01:43 +0530

    Update 0202-happy-number.java

--- Leetcode solutions/0202-happy-number/0202-happy-number.java
+++ Leetcode solutions/0202-happy-number/0202-happy-number.java
@@ -1,24 +1,12 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1)
-        {
-            return true;
-        }
-        if(n<=9 &&n%2==0)
-        {
-            return false;
-        }
-        int rem,sum=0;
-        while(n>0)
-        {
-            rem=n%10;
-            sum=sum+rem*rem;
-            n=n/10;
-        }
-        return isHappy(sum);
-    
-        
-           
-        
-    }
+     while(n!=1 && n!=4){
+        int sum=0;
+        while(n>0){
+            int r=n%10;
+            sum+=r*r;
+            n/=10;
+        }n=sum;
+    }return n==1;
+  }
 }
