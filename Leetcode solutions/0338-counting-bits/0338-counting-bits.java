c35773d39ec6c892d8ed597a0df24c8ee80aa54e
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-19 11:21:21 +0530

    Update 0338-counting-bits.java

--- Leetcode solutions/0338-counting-bits/0338-counting-bits.java
+++ Leetcode solutions/0338-counting-bits/0338-counting-bits.java
@@ -1,25 +1,36 @@
 class Solution {
     public int[] countBits(int n) {
-        int ans[]=new int[n+1];
-        for(int i=0;i<=n;i++)
-        {
-            String str=Integer.toBinaryString(i);
-            ans[i]=count(str);
+        int a[] = new int[n + 1];   
+        for (int i = 1; i <= n; i++) {
+            a[i] = a[i / 2] + (i % 2); 
         }
-        return ans;
-        
-        
-    }
-    int count(String s)
-    {
-        int d=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='1')
-            {
-                d++;
-            }
-        }
-        return d;
+        return a;
     }
 }
+
+/*
+int[] a = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            a[i] = Integer.bitCount(i); 
+        }
+        return a;
+    }
+}
+*/
+
+/*
+int a[] = new int[n + 1];   
+        for (int i = 0; i <= n; i++) { 
+            String b = Integer.toBinaryString(i); 
+            int count = 0;     
+            for (int j = 0; j < b.length(); j++) { 
+                if (b.charAt(j) == '1') { 
+                    count++;
+                }
+            }
+            a[i] = count; 
+        }
+        return a;
+    }
+}
+*/
