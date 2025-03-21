3c0b859514eb24c965f7d9d8af43ee26a19ff522
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-21 20:51:37 +0530

    Update 287-FindtheDuplicateNumber.java

--- Leetcode solutions/0287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ Leetcode solutions/0287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,14 +1,11 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        //Add in Hashmap && verifcy if value is greated than 1
-        for(int i=0; i<n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])>1){
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1])
+            {
                 return nums[i];
             }
-        }
-        return -1;
+        }return -1;
     }
 }
