0920133f6ce845a65d625127f53b6b8c8bedfec2
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-23 14:28:16 +0530

    Update and rename 01.Integer to Binary.txt to 01.Integer to Binary.java

--- 08.Bit Manipulation/01.Integer to Binary.java
+++ 08.Bit Manipulation/01.Integer to Binary.java
@@ -33,3 +33,19 @@ Processing Steps:
 
 Output:
 Binary: 1010
+
+======================================================
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=8;
+	    String s = "";
+	    while(n>0){
+	        int r = n%2;
+	        s=r+s;
+	        n=n/2;
+	    }
+		System.out.println(s);
+	}
+}
