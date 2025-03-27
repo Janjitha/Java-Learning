f7cc6a88a36b8a8e83fcc60404b580704416e335
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-27 19:59:26 +0530

    Update 0268-missing-number.java

--- Leetcode solutions/0268-missing-number/0268-missing-number.java
+++ Leetcode solutions/0268-missing-number/0268-missing-number.java
@@ -1,5 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int n = nums.length;  //to find missing take one element extra
+        int expected = n*(n+1)/2;
+        int actual = 0;
+        for(int s : nums){
+            actual+=s;
+        }
+        int ans = expected - actual;
+        return ans;
+    }
+}
+
+/*
+class Solution {
+    public int missingNumber(int[] nums) {
         int n = nums.length;
         int sum = n * (n + 1) / 2;
         for (int num : nums) 
@@ -7,3 +21,4 @@ class Solution {
         return sum;
     }
 }
+*/
