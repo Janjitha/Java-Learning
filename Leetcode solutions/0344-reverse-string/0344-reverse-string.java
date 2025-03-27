5af6102aea617153c781c2a747ed8294640d46b9
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-27 19:58:19 +0530

    Update 0344-reverse-string.java

--- Leetcode solutions/0344-reverse-string/0344-reverse-string.java
+++ Leetcode solutions/0344-reverse-string/0344-reverse-string.java
@@ -1,5 +1,21 @@
 class Solution {
     public void reverseString(char[] s) {
+        //using 2 pointer method
+        int l=0;
+        int r=s.length-1;
+        while(l<r){ //check until both pointers meet at the same place
+            char temp = s[l]; //swap the values
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
+
+/*
+class Solution {
+    public void reverseString(char[] s) {
         int n=s.length;
        // int j;
         for(int i=0,j=n-1;i<j;i++,j--)
@@ -11,3 +27,4 @@ class Solution {
         
     }
 }
+*/
