b92085af2108d28d3c8eeb1952045c705d5f1575
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-27 20:03:21 +0530

    Update 0079-word-search.java

--- Leetcode solutions/0079-word-search/0079-word-search.java
+++ Leetcode solutions/0079-word-search/0079-word-search.java
@@ -1,4 +1,30 @@
 class Solution {
+    public boolean exist(char[][] b, String word) 
+    {
+        for(int r = 0 ; r < b.length ; r++)
+            for(int c = 0 ; c < b[0].length ; c++)
+                if(b[r][c] == word.charAt(0) && help(b,word,0,r,c))
+                    return true;
+        return false;
+    }    
+    public boolean help(char[][] b, String word, int start, int r, int c)
+    {
+        if(word.length() <= start)
+            return true;        
+        if(r < 0 || c < 0 || r >= b.length || c >= b[0].length || 
+        b[r][c] == '0' || b[r][c] != word.charAt(start))
+            return false;       
+        char tmp = b[r][c];
+        b[r][c] = '0';
+        if(help(b,word,start+1,r+1,c) || help(b,word,start+1,r-1,c) || help(b,word,start+1,r,c+1) || help(b,word,start+1,r,c-1))
+            return true;      
+        b[r][c] = tmp;
+        return false;
+    }
+}
+
+/*
+class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
@@ -16,15 +42,12 @@ class Solution {
         if(len == word.length()){
             return true;
         }
-
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length){
             return false;
         }
-
         if(board[i][j] != word.charAt(len)){
             return false;
         }
-
         char temp = board[i][j];
         board[i][j] = '*';
 
@@ -34,7 +57,7 @@ class Solution {
                         help(board, i, j+1, word, len+1);
 
         board[i][j] = temp;
-
         return found;
     }
 }
+*/
