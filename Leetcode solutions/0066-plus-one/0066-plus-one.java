61bcbde91e95feb6abd93b93d1b9c1b55c40b422
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-27 19:55:57 +0530

    Update 0066-plus-one.java

--- Leetcode solutions/0066-plus-one/0066-plus-one.java
+++ Leetcode solutions/0066-plus-one/0066-plus-one.java
@@ -1,5 +1,23 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++; // If not 9, increment and return
+                return digits;
+            }
+            digits[i] = 0; // If 9, set to 0 and continue loop
+        }
+        // If all digits were 9, we need extra digit
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1; // Example: [9,9,9] → [1,0,0,0]
+        return newNumber;
+    }
+}
+
+/*
+class Solution {
+    public int[] plusOne(int[] digits) {
            for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] != 9) {
                 digits[i]++;
@@ -12,3 +30,4 @@ class Solution {
         return result;
     }
 }
+*/
