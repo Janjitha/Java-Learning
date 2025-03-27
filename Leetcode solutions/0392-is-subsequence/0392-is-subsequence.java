25bbf88ccd1b341da895295086eda9386fa5c47a
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-27 20:01:28 +0530

    Update 0392-is-subsequence.java

--- Leetcode solutions/0392-is-subsequence/0392-is-subsequence.java
+++ Leetcode solutions/0392-is-subsequence/0392-is-subsequence.java
@@ -1,8 +1,23 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) return false; // Quick check
+        int i = 0;
+        int j = 0;
+        while (j < t.length() && i < s.length()) {
+            if (s.charAt(i) == t.charAt(j)) 
+            i++; 
+            j++;
+            
+        }
+        return i == s.length();
+    }
+}
+
+/*
+class Solution {
+    public boolean isSubsequence(String s, String t) {
         int i=0;
         int j=0;
-
         int n1 = s.length();
         int n2 = t.length();
 
@@ -17,3 +32,4 @@ class Solution {
         return i == n1;
     }
 }
+*/
