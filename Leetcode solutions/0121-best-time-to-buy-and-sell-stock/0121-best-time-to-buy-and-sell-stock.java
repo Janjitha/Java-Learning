decb0b919a29e16c8fe13ed2f03be9c631968b09
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-03-30 14:00:16 +0530

    Update 0121-best-time-to-buy-and-sell-stock.java

--- Leetcode solutions/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ Leetcode solutions/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;  // Track the minimum price
+        int maxProfit = 0;  // Track the maximum profit 
+        for (int price : prices) {
+            min = Math.min(min, price);  // Update min price
+            maxProfit = Math.max(maxProfit, price - min);  // Update max profit
+        }
+        return maxProfit;
+    }
+}
+
+//         int min = Integer.MAX_VALUE; // Track min price
+//         int maxProfit = 0; // Track max profit   
+//         for (int price : prices) {
+//             if (price < min) { 
+//                 min = price; // Update min price if we find a new low
+//             } else {
+//                 maxProfit = Math.max(maxProfit, price - min); // Calculate profit
+//             }
+//         }
+//         return maxProfit;
+//     }
+// }
+
+/*
+class Solution {
+    public int maxProfit(int[] prices) {
         int min = prices[0];
         int profit = 0;
         int cost = 0;
@@ -12,3 +38,4 @@ class Solution {
         return profit;
     }
 }
+*/
