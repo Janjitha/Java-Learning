e85e226912f735f6f6fba641dbcee43a5d84c0c8
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-04-08 07:19:55 +0530

    Update 0043-multiply-strings.java

--- Leetcode solutions/0043-multiply-strings/0043-multiply-strings.java
+++ Leetcode solutions/0043-multiply-strings/0043-multiply-strings.java
@@ -1,11 +1,37 @@
-import java.math.*;
-class Solution {
-    public String multiply(String num1, String num2) {
-       BigInteger a=new BigInteger(num1);
-       BigInteger b=new BigInteger(num2);
-       BigInteger c=a.multiply(b);
-       String ans=c.toString();
-       return ans;
-        
+        BigInteger x = new BigInteger(num1);
+        BigInteger y = new BigInteger(num2);
+        BigInteger res = x.multiply(y);
+        return res.toString();
     }
 }
+
+/*
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+			return "0";
+		}
+		char[] ch1 = num1.toCharArray();
+		char[] ch2 = num2.toCharArray();
+		int l1 = num1.length(), l2 = num2.length();
+		int[] res = new int[l1 + l2];
+		for (int i = l2 - 1; i >= 0; i--) {
+			for (int j = l1 - 1; j >= 0; j--) {
+				int product = (ch1[j] - '0') * (ch2[i] - '0');
+				int current = product + res[i + j + 1];
+				res[i + j + 1] = current % 10;
+				res[i + j] += current / 10;
+			}
+		}
+		int index = 0;
+		while (index < res.length && res[index] == 0) {
+			++index;
+		}
+		String result = "";
+		for (; index < res.length; index++) {
+			result += String.valueOf(res[index]);
+		}
+		return result;
+	}
+}
+*/
