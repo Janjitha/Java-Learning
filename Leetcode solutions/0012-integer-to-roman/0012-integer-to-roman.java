0f1581a0c76e9c022e4d5013d4351d6a5de400fd
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-04-08 07:23:01 +0530

    Update 0012-integer-to-roman.java

--- Leetcode solutions/0012-integer-to-roman/0012-integer-to-roman.java
+++ Leetcode solutions/0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,17 +1,9 @@
 class Solution {
     public String intToRoman(int num) {
-        
-        int[] intCode = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] code = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<intCode.length; i++){
-            while(num >= intCode[i]){
-                sb.append(code[i]);
-                num -= intCode[i];
-            }
-        }
-        return sb.toString();
+        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};  //I -> X and V -> L and for last consider next one
+        String hund[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};  
+        String thou[] = {"","M","MM","MMM"};
+        return thou[num/1000] + hund[(num%1000)/100]+tens[(num%100)/10]+ones[num%10];
     }
 }
