d02b3ab2e0042f7b850909ec8429882cd9b558a9
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-06-20 10:43:27 +0530

    Update 3. Retrive a element.java

--- 11. ArrayList/3. Retrive a element.java
+++ 11. ArrayList/3. Retrive a element.java
@@ -4,4 +4,31 @@ Write a Java program to retrieve an element (at a specified index) from a given 
 
 PROGRAM :
 
-  
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<String> l = new ArrayList<>();
+		l.add("White");
+		l.add("Red");
+		l.add("Blue");
+		l.add("Pink");
+	  l.add(0,"Black");
+	  l.add(2,"Sandal");
+	  l.add(l.size(),"Gray");
+	  System.out.println("The l contains : " +l);
+	  String first = l.get(0);
+	  String second = l.get(2);
+	  String last = l.get(l.size()-1);
+	  System.out.println("First : " +first);
+	  System.out.println("Second : " +second);
+	  System.out.println("Last : " +last);
+	}
+}
+
+OUTPUT :
+
+The l contains : [Black, White, Sandal, Red, Blue, Pink, Gray]
+First : Black
+Second : Sandal
+Last : Gray
