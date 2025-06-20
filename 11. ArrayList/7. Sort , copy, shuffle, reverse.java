fcb333b1baa5c75667b4e331300091a475327ae1
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-06-20 11:00:39 +0530

    Create 7. Sort , copy, shuffle, reverse.java

---
+++ 11. ArrayList/7. Sort , copy, shuffle, reverse.java
@@ -0,0 +1,36 @@
+QUESTION :
+
+1. Write a Java program to sort a given array list.
+2. Write a Java program to copy one array list into another.
+3. Write a Java program to shuffle elements in an array list.
+4. Write a Java program to reverse elements in an array list.
+
+PROGRAM :
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<String> l = new ArrayList<>();
+		l.add("White");
+		l.add("Red");
+		l.add("Blue");
+		l.add("Pink");
+		System.out.println("Orginal : " +l);
+		Collections.sort(l);
+		System.out.println("Sorted String : "+l);
+		// it shuffles randomly
+		Collections.shuffle(l);
+		System.out.println("Shuffled String : " +l);
+		// to reverse the array with string or int
+		Collections.reverse(l);
+		System.out.println("Reverse String : "+l);
+	}
+}
+
+OUTPUT :
+
+Orginal : [White, Red, Blue, Pink]
+Sorted String : [Blue, Pink, Red, White]
+Shuffled String : [Blue, Pink, White, Red]
+Reverse String : [Red, White, Pink, Blue]
